5dc7329d4c72cee01cc05268a7c34f981832a9fa
administrator <administrator@DOMAINPC>
2018-04-12 11:57:12 +0700

    blogpage and item blogpage

--- Ms.Ngoc/App_Data/Models/models.generated.cs
+++ Ms.Ngoc/App_Data/Models/models.generated.cs
@@ -20,7 +20,7 @@ using Umbraco.ModelsBuilder.Umbraco;
 
 [assembly: PureLiveAssembly]
 [assembly:ModelsBuilderAssembly(PureLive = true, SourceHash = "57c6c889213927a3")]
-[assembly:System.Reflection.AssemblyVersion("0.0.0.4")]
+[assembly:System.Reflection.AssemblyVersion("0.0.0.8")]
 
 namespace Umbraco.Web.PublishedContentModels
 {
