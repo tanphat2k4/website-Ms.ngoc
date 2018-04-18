31558ab57b36c2dda4cde662077bc9a9840b514a
administrator <administrator@DOMAINPC>
2018-04-18 12:28:41 +0700

    next

--- Ms.Ngoc/App_Data/Models/models.generated.cs
+++ Ms.Ngoc/App_Data/Models/models.generated.cs
@@ -20,7 +20,7 @@ using Umbraco.ModelsBuilder.Umbraco;
 
 [assembly: PureLiveAssembly]
 [assembly:ModelsBuilderAssembly(PureLive = true, SourceHash = "d402a4a99095ec86")]
-[assembly:System.Reflection.AssemblyVersion("0.0.0.6")]
+[assembly:System.Reflection.AssemblyVersion("0.0.0.1")]
 
 namespace Umbraco.Web.PublishedContentModels
 {
--- Ms.Ngoc/js/Slider/site.js
+++ Ms.Ngoc/js/Slider/site.js
@@ -1,11 +1,32 @@
 ﻿$(function () {
+    $('#myGalleryProject').galleryView({
+        panel_width: 486,
+        panel_height: 300
+    });
+
     $('#jcarousel')
-        .jcarousel({
-            wrap: 'circular'
-        })
-        .jcarouselAutoscroll({
-            interval: 2000,
-            target: '+=1',
-            autostart: true
+       .jcarousel({
+           wrap: 'circular'
+       })
+       .jcarouselAutoscroll({
+           interval: 2000,
+           target: '+=1',
+           autostart: true
+       });
+    $(".tabButton").each(function () {
+        $(this).click(function () {
+            $(".tabButton").removeClass("current");
+            $("#projectDetail").html($(this).siblings(".tabContent").html());
+            $(this).addClass("current");
         });
+    });
+
+    var url = window.location.href;
+    $("#menu").find("a").each(function() {
+        var $self = $(this);
+        if (url.indexOf($self.attr("href")) > 1) {
+            $("#menu").find("li").removeClass("menu_active");
+            $self.closest("li").addClass("menu_active");
+        }
+    });
 });
