4c4293f78411d40f553c08e04a50389bda028f22
administrator <administrator@DOMAINPC>
2018-04-19 17:24:03 +0700

    next

--- Ms.Ngoc/App_Data/Models/all.generated.cs
+++ Ms.Ngoc/App_Data/Models/all.generated.cs
@@ -9,7 +9,7 @@ using  Umbraco.ModelsBuilder;
 using  Umbraco.ModelsBuilder.Umbraco;
 [assembly: PureLiveAssembly]
 [assembly:ModelsBuilderAssembly(PureLive = true, SourceHash = "49bd4a9fc908bbf7")]
-[assembly:System.Reflection.AssemblyVersion("0.0.0.5")]
+[assembly:System.Reflection.AssemblyVersion("0.0.0.1")]
 
 
 // FILE: models.generated.cs
